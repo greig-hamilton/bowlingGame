4047b11bba5b7626c6a0454fb42eadff366bcb7c
Greig Hamilton <greig.hamilton@gmail.com>
2021-04-09 20:59:06 +1200

    Add extra comments and restrict tests to /src subdirectory

--- src/bowlingGame.ts
+++ src/bowlingGame.ts
@@ -1,7 +1,15 @@
 // Write code for a bowling game.
-// The roll function should be called each time a player rolls the ball
-// The getScoreForCompleteGame should be called once the game is complete
-// and will return the final score.
+//
+// The roll() function should be called each time a player rolls the ball
+// The getScoreForCompleteGame() should be called only once the game is
+// complete and will return the final score.
+//
+// For example to simulate a game where a player knocks down 1 pin each roll
+// you should call roll(1) 20 times (10 frames and two rolls per frame). This
+// will be a complete game. Then you can call getScoreForCompleteGame().
+// A second example is if a player gets a strike on every roll. This game would
+// be roll(10) for 12 rolls (10 frames with 1 roll + 2 extra rolls in the 10th
+// frame).
 //
 // If time allows extend the functionality to support getting a score for
 // a game that is still in progress.
